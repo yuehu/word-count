7ddf35b486fcdbec8cdc598b759647551c1b1d6a
Hsiaoming Yang <me@lepture.com>
2014-02-10 23:14:52 +0800

    Init commits

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# word-count
+
+  Word count in respect of CJK characters.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install yuehu/word-count
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Word Count
+ *
+ * Word count in respect of CJK characters.
+ *
+ * Copyright (c) 2014 by Hsiaoming Yang.
+ */
+
+var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
+
+module.exports = function (data) {
+  var m = data.match(pattern);
+  var count = 0;
+  for (var i = 0; i < m.length; i++) {
+    if (m[i].charCodeAt(0) >= 0x4E00) {
+      count += m[i].length;
+    } else {
+      count += 1;
+    }
+  }
+  return count;
+};
