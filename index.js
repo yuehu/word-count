f4af741512cfa0d15b3385836b413c76c2a49023
Hsiaoming Yang <me@lepture.com>
2014-02-10 23:57:31 +0800

    Use lower case

--- index.js
+++ index.js
@@ -6,13 +6,13 @@
  * Copyright (c) 2014 by Hsiaoming Yang.
  */
 
-var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
+var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
 
 module.exports = function (data) {
   var m = data.match(pattern);
   var count = 0;
   for (var i = 0; i < m.length; i++) {
-    if (m[i].charCodeAt(0) >= 0x4E00) {
+    if (m[i].charCodeAt(0) >= 0x4e00) {
       count += m[i].length;
     } else {
       count += 1;
