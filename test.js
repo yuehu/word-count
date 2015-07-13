9dfabd05c43bb875b438cd018ffcf7d0f92a46c6
Hsiaoming Yang <me@lepture.com>
2015-07-13 10:33:22 +0800

    Fix on counting Arabic

--- test.js
+++ test.js
@@ -7,12 +7,27 @@ describe('word count', function() {
     }
   }
 
-  it('should count right', function() {
+  it('should count CJK at end', function() {
     assert(count('this is 中文'), 4);
+  });
+
+  it('should count CJK at start', function() {
     assert(count('中文is this'), 4);
+  });
+
+  it('should count CJK mix', function() {
     assert(count('this中文is a 单词'), 7);
+  });
+
+  it('should count Arabic', function() {
     assert(count('سلام سلام.'), 2);
+  });
+
+  it('should count Arabic and English', function() {
     assert(count('this لغة'), 2);
+  });
+
+  it('should count Arabic 3 words', function() {
     assert(count('داد فارسی ۱۲۳۱۲۳'), 3);
   });
 
