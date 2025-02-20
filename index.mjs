ed5aef34eb4ebb7316055967398f685d49e60d41
Hsiaoming Yang <me@lepture.com>
2025-02-20 17:49:38 +0900

    chore: release 0.3.0

--- index.mjs
+++ index.mjs
@@ -9,6 +9,7 @@
 const pattern = /[a-zA-Z0-9_\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff\u0400-\u04ff]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
 
 /**
+ * Calculate word count in respect of CJK characters.
  *
  * @param {string} text
  * @returns {number}
